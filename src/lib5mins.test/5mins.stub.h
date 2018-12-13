61cc9b1047ce80ca8d2efed70fdcb1aaf34a6bb6
kalaider <a.kalaider@yandex.ru>
2018-12-13 20:28:38 +0300

    test(lib5mins): link real library

--- src/lib5mins.test/5mins.stub.h
+++ src/lib5mins.test/5mins.stub.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <5mins/TimeConverter.h>
+
 /* stub implementation */
 
-inline int GetTimePlus5MinStr(std::string init_time, std::string & new_time) { return 0; }
+inline int GetTimePlus5MinStr(std::string init_time, std::string & new_time) { return TimeConverter::GetTimePlus5MinStr(std::move(init_time), new_time); }
