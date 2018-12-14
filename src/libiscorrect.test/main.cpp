7737dd8c9ab1887e5f43a01cad1d530260717019
kalaider <a.kalaider@yandex.ru>
2018-12-14 18:31:17 +0300

    test(libiscorrect): add xml output option

--- src/libiscorrect.test/main.cpp
+++ src/libiscorrect.test/main.cpp
@@ -3,6 +3,20 @@
 #include "tests/iscorrect.test.h"
 
 int main(int argc, char **argv) {
+    bool xml = false;
+
+    if (argc > 1)
+    {
+        if (std::strcmp(argv[1], "-a") == 0) xml = true;
+        if (std::strcmp(argv[1], "-b") == 0) xml = false;
+    }
+
+    if (xml)
+    {
+        ::testing::GTEST_FLAG(output) = "xml:report.xml";
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
